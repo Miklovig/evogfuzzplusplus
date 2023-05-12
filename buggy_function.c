0bda5cf5616acdf75ed77cc5ffef869db7bd4057
Miklovig <miklovig@informatik.hu-berlin.de>
2023-05-12 19:08:04 +0200

    dumbed down inputs / results perffuzz

--- buggy_function.c
+++ buggy_function.c
@@ -7,11 +7,12 @@ typedef struct {
     char* data;
 } DerivationTree;
 
-void buggy_function(DerivationTree* inp) {
-    char* str = inp->data;
+void buggy_function(char* inp) {
+    char* str = inp;
     int bigNumber = 1;
 
     if (strstr(str, "mem") != NULL) {
+        printf("Simulating high memory usage\n");
         // Simulate high memory usage
         int bigList = 0;
         for (int i = 0; i < 10000; i++) {
@@ -19,6 +20,7 @@ void buggy_function(DerivationTree* inp) {
         }
     }
     if (strstr(str, "cpu") != NULL) {
+        printf("Simulating high CPU usage\n");
         // Simulate high CPU usage
         int i = 0;
         while (i < 10000) {
@@ -27,15 +29,18 @@ void buggy_function(DerivationTree* inp) {
         }
     }
     if (strstr(str, "time") != NULL) {
+        printf("Simulating high time usage\n");
         // Simulate high time usage
         clock_t start_time = clock();
         while ((double)(clock() - start_time) / CLOCKS_PER_SEC < 0.1) {}
     }
     if (strstr(str, "cov") != NULL) {
+        printf("Simulating more lines for coverage testing\n");
         // Simulate more lines for coverage testing
         int doSomething = 1;
     }
     if (strstr(str, "error") != NULL) {
+        printf("Simulating TypeError\n");
         // Simulate TypeError
         char* errorMsg = "TypeError been thrown";
         int len = strlen(errorMsg);
@@ -44,9 +49,11 @@ void buggy_function(DerivationTree* inp) {
         str_copy[len] = '\0';
         perror(str_copy);
         free(str_copy);
-        exit(1);
+        // exit(1);
+        return;
     }
     if (strstr(str, "exception") != NULL) {
+        printf("Simulating ValueError\n");
         // Simulate ValueError
         char* errorMsg = "ValueError been thrown";
         int len = strlen(errorMsg);
@@ -55,6 +62,44 @@ void buggy_function(DerivationTree* inp) {
         str_copy[len] = '\0';
         perror(str_copy);
         free(str_copy);
-        exit(1);
+        // exit(1);
+        return;
     }
 }
+
+#define MAX_INPUTS 100
+
+int main(int argc, char* argv[]) {
+    FILE* file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return 1;
+    }
+
+    char inputs[MAX_INPUTS][100];  // Assuming each input has a maximum length of 100 characters
+    int numInputs = 0;
+
+    // Read each line from the file and store it in the inputs array
+    char line[100];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        // Remove newline character at the end of the line
+        if (line[strlen(line) - 1] == '\n') {
+            line[strlen(line) - 1] = '\0';
+        }
+
+        // Copy the line to the inputs array
+        strcpy(inputs[numInputs], line);
+        numInputs++;
+    }
+
+    fclose(file);
+
+    // Call the function for each input
+    for (int i = 0; i < numInputs; i++) {
+        // Call your function here using inputs[i]
+        buggy_function(inputs[i]);
+    }
+
+    return 0;
+}
+
